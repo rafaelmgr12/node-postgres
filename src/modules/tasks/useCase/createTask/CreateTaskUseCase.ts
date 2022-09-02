a378ae20e2e83f036b24584425332a3fadd97241
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-09-02 18:03:19 -0300

    fix: refactoring

--- src/modules/tasks/useCase/createTask/CreateTaskUseCase.ts
+++ src/modules/tasks/useCase/createTask/CreateTaskUseCase.ts
@@ -1,23 +1,18 @@
-import { prisma } from "../../../../config/prismaClient";
-import { ITaskDTO } from "../../dtos/ITask";
+import { ICreateTaskDTO } from "../../dtos/ITask";
 import { TaskRepository } from "../../repository/TaskRepository";
 
 export class CreateTaskUseCase {
-  async execute({ title, description }: ITaskDTO) {
+  async execute({ title, description }: ICreateTaskDTO) {
     const taskRepository = new TaskRepository();
-    const taskAlreadyExists = await taskRepository.findByTitle({
-      title,
-      description,
-    });
+    const taskAlreadyExists = await taskRepository.findByTitle({ title });
     if (taskAlreadyExists) {
       throw new Error("Task already exists!");
     }
-
-    return prisma.task.create({
-      data: {
-        title,
-        description,
-      },
+    const task = await taskRepository.createTask({
+      title,
+      description,
     });
+
+    return task;
   }
 }

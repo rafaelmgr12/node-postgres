f78b52d5bf4ca35208ded2786880f17888ab07dd
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2022-09-02 18:02:43 -0300

    feat: new Dtos

--- src/modules/tasks/dtos/ITask.ts
+++ src/modules/tasks/dtos/ITask.ts
@@ -1,7 +1,19 @@
 export interface ITaskDTO {
   id?: number;
   title: string;
-  description: string;
+  description?: string;
   completed?: boolean;
   updated_at?: Date;
+}
+
+export interface ICreateTaskDTO {
+  title: string;
+  description?: string;
+}
+
+export interface IUpdateTaskDTO {
+  id: number;
+  title: string;
+  completed: boolean | false;
+  description?: string;
 }

c25dc6a79a36786b9110ae95f42b85c9652d15d4
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-09-09 10:07:45 -0300

    feat: add a new rule for create task

--- src/modules/tasks/useCase/createTask/CreateTaskUseCase.ts
+++ src/modules/tasks/useCase/createTask/CreateTaskUseCase.ts
@@ -4,10 +4,6 @@ import { TaskRepository } from "../../repository/TaskRepository";
 export class CreateTaskUseCase {
   async execute({ title, description }: ICreateTaskDTO) {
     const taskRepository = new TaskRepository();
-    const taskAlreadyExists = await taskRepository.findByTitle({ title });
-    if (taskAlreadyExists) {
-      throw new Error("Task already exists!");
-    }
     const task = await taskRepository.createTask({
       title,
       description,
